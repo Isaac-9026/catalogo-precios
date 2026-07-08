d21bf5ce30f52982e07394238d4b1a1dcc8bdcc6
Soy Isaac <isaipaucarchuquispuma@gmail.com>
2026-07-07 21:48:32 -0500

    feat(core): inicializar supabase y proteger anon key en gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
-import 'features/search/presentation/search_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'core/database/database_helper.dart';
+import 'features/search/presentation/search_screen.dart';
+import 'core/supabase/env.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
+  await Supabase.initialize(
+    url: Env.supabaseUrl,
+    anonKey: Env.supabaseAnonKey,
+  );
+
+  // PRUEBA TEMPORAL DE CONEXIÓN A SUPABASE (FASE 5)
+  try {
+    final response = await Supabase.instance.client.from('products').select();
+    debugPrint('=== SUPABASE CONNECTION TEST SUCCESS ===');
+    debugPrint(response.toString());
+    debugPrint('=========================================');
+  } catch (e) {
+    debugPrint('=== SUPABASE CONNECTION TEST FAILED ===');
+    debugPrint(e.toString());
+    debugPrint('========================================');
+  }
+
   // Initialize Database
   await DatabaseHelper.instance.database;
 
